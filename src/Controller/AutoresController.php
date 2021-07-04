51ddcc4689cb8a8d54d8ba795b045fa76d16670b
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2021-07-04 08:55:08 +0200

    Crear Controlador Autores

---
+++ src/Controller/AutoresController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AutoresController extends AbstractController
+{
+    /**
+     * @Route("/autores", name="autores")
+     */
+    public function index(): Response
+    {
+        return $this->render('autores/index.html.twig', [
+            'controller_name' => 'AutoresController',
+        ]);
+    }
+}
