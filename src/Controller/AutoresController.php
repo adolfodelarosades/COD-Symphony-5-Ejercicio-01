bc6403db885389c0ba4f7b36748e965d6f1d6e3f
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2021-07-04 21:49:09 +0200

    Diseñar la Página de las Autores

--- src/Controller/AutoresController.php
+++ src/Controller/AutoresController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AutorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class AutoresController extends AbstractController
     /**
      * @Route("/autores", name="autores")
      */
-    public function index(): Response
+    public function index(AutorRepository $autorRepository): Response
     {
+        $autores = $autorRepository->findAll();
+
         return $this->render('autores/index.html.twig', [
-            'controller_name' => 'AutoresController',
+            'autores' => $autores
         ]);
     }
 }
