73a3a9ef1846125c3ac02765e3862499a49db2ee
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2021-07-04 22:16:05 +0200

    Diseñar la Página de las Fondos

--- src/Controller/FondosController.php
+++ src/Controller/FondosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FondoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class FondosController extends AbstractController
     /**
      * @Route("/fondos", name="fondos")
      */
-    public function index(): Response
+    public function index(FondoRepository $fondoRepository): Response
     {
+        $fondos = $fondoRepository->findAll();
+
         return $this->render('fondos/index.html.twig', [
-            'controller_name' => 'FondosController',
+            'fondos' => $fondos
         ]);
     }
 }
